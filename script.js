5bf438a2cdc36a1f2c8cdef3f6b8d4bd0b86069a
Friel2021 <87154134+Friel2021@users.noreply.github.com>
2021-08-01 21:24:32 -0600

    Update script.js
    Corrected code for lowercaseCheck

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var passwordLength;
 var uppercaseCheck;
+var lowercaseCheck;
 var numberCheck;
 var specialCheck;
 
